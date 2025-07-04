43b332cdd5fda6b167886e8c73fde734106b61c8
mohankodela <kodelamohankrishna999@gmail.com>
2025-07-04 19:02:01 +0530

    roboshop

--- Redis.sh
+++ Redis.sh
@@ -45,8 +45,6 @@ echo "Updating Config Files"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf &>>$LOGFILE
 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf &>>$LOGFILE
-
 VALIDATE $? "Updating Config Files"
 
 systemctl enable redis &>>$LOGFILE
