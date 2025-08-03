4ee771f5c740d9590e61c79f95be023d6ddfb159
mohankodela <kodelamohankrishna999@gmail.com>
2025-08-03 09:26:44 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -14,9 +14,11 @@ do
     else
         INSTANCE_TYPE="t2.micro"
     fi
-
-    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
-    echo "$i: $IP_ADDRESS"
+    
+    if [ $i != "web"]
+    then
+    PRI_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    echo "$i: $PRI_ADDRESS"
 
     #create R53 record, make sure you delete existing record
     aws route53 change-resource-record-sets \
@@ -31,10 +33,34 @@ do
             ,"Type"             : "A"
             ,"TTL"              : 1
             ,"ResourceRecords"  : [{
-                "Value"         : "'$IP_ADDRESS'"
+                "Value"         : "'$PRI_ADDRESS'"
             }]
         }
         }]
     }
         '
+    else
+    PUB_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PublicIpAddress' --output text)
+    echo "$i: $PUB_ADDRESS"
+
+    #create R53 record, make sure you delete existing record
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Creating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$i'.'$DOMAIN_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$PUB_ADDRESS'"
+            }]
+        }
+        }]
+    }
+        '
+    fi    
 done
