8e196e512ecbed58858b21b4bcce1dbe32133942
mohankodela <kodelamohankrishna999@gmail.com>
2025-08-03 09:45:07 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -15,8 +15,6 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
     
-    if [ $i != "web" ]
-    then
     PRI_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i: $PRI_ADDRESS"
 
@@ -39,28 +37,4 @@ do
         }]
     }
         '
-    else
-    PUB_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --associate-public-ip-address --query 'Instances[0].PublicIpAddress' --output text)
-    echo "$i: $PUB_ADDRESS"
-
-    #create R53 record, make sure you delete existing record
-    aws route53 change-resource-record-sets \
-    --hosted-zone-id $ZONE_ID \
-    --change-batch '
-    {
-        "Comment": "Creating a record set for cognito endpoint"
-        ,"Changes": [{
-        "Action"              : "UPSERT"
-        ,"ResourceRecordSet"  : {
-            "Name"              : "'$i'.'$DOMAIN_NAME'"
-            ,"Type"             : "A"
-            ,"TTL"              : 1
-            ,"ResourceRecords"  : [{
-                "Value"         : "'$PUB_ADDRESS'"
-            }]
-        }
-        }]
-    }
-        '
-    fi    
 done
